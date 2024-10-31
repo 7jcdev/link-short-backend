65fb4c1e0c9c33e0633b9b48763e1cfca80facb6
7jcdev <7jaydev@protonmail.com>
2024-10-30 22:09:40 -0600

    changed cors

--- src/server.js
+++ src/server.js
@@ -27,9 +27,9 @@ const corsUrl = [process.env.FRONTEND_URI];
 }
 app.use(cors(corsOptions));    
 */
-
+// [`${corsUrl}`]
 app.use((req, res, next)=>{
-    res.append("Access-Control-Allow-Origin", [`${corsUrl}`])
+    res.append("Access-Control-Allow-Origin", ["*"])
     res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE")
     res.append('Access-Control-Allow-Headers', 'Content-Type');
     res.append('Access-Control-Allow-Credentials', true)
